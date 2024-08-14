a8a38c8a304307d037bf972ad4326c85da6d785d
Sofi Brugo <brugosofi@gmail.com>
2024-08-14 10:35:49 -0300

    changes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+//variables
+const proyectosJavascript = document.querySelector('.details-container');
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open")
 }
+
+proyectosJavascript.addEventListener('click', function(){
+    console.log("funciona onclick");
+})
